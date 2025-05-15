ebdc7b901cf53b24944b9dd48164e380709acb6c
KrisnahadiJ <krisnahadij@gmail.com>
2025-05-15 16:02:48 +0700

    2.2 Modifikasi Elemen pada Single Linked List

--- Pertemuan12/SLLMain11.java
+++ Pertemuan12/SLLMain11.java
@@ -1,34 +1,30 @@
 package Pertemuan12;
 
-import java.util.Scanner;
-
 public class SLLMain11 {
     public static void main(String[] args) {
         SingleLinkedList11 sll = new SingleLinkedList11();
-        Scanner sc = new Scanner(System.in);
 
-        System.out.print("Masukkan jumlah data yang ingin ditambahkan:");
-        int jumlahData = sc.nextInt();
-        sc.nextLine(); 
-        for (int i = 0; i < jumlahData; i++) {
-            System.out.println("Masukkan data ke-" + (i + 1) + ":");
-            System.out.print("NIM: ");
-            String nim = sc.nextLine();
-            System.out.print("Nama: ");
-            String nama = sc.nextLine();
-            System.out.print("Kelas: ");
-            String kelas = sc.nextLine();
-            System.out.print("IPK: ");
-            double ipk = sc.nextDouble();
-            sc.nextLine(); 
+        Mahasiswa11 mhs1 = new Mahasiswa11("21212203", "Dirga", "4D", 3.6);
+        Mahasiswa11 mhs2 = new Mahasiswa11("22212202", "Cintia", "3C", 3.5);
+        Mahasiswa11 mhs3 = new Mahasiswa11("23212201", "Bimon", "2B", 3.8);
+        Mahasiswa11 mhs4 = new Mahasiswa11("24212200", "Alvaro", "1A", 4.0);
 
-            Mahasiswa11 mhs = new Mahasiswa11(nim, nama, kelas, ipk);
-            sll.addLast(mhs); 
-        }
+        sll.addLast(mhs1);
+        sll.addLast(mhs2);
+        sll.addLast(mhs3);
+        sll.addLast(mhs4);
 
-        System.out.println("\nData dalam Linked List:");
+        System.out.println("Data pada indeks 1:");
+        sll.getData11(1);
+
+        System.out.println("Data mahasiswa bernama Bimon berada pada indeks: " + sll.indexOf("Bimon"));
+        System.out.println();
+
+        sll.removeFirst();
+        sll.removeLast();
         sll.print();
 
-        sc.close();
+        sll.removeAt(0);
+        sll.print();
     }
 }
