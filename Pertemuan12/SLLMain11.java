5a9d287d367d11cc511688a56ad81c8ee4727685
KrisnahadiJ <krisnahadij@gmail.com>
2025-05-15 15:01:27 +0700

    Modifikasi Nomor 3 Percobaan 1

--- Pertemuan12/SLLMain11.java
+++ Pertemuan12/SLLMain11.java
@@ -1,24 +1,34 @@
 package Pertemuan12;
 
+import java.util.Scanner;
+
 public class SLLMain11 {
     public static void main(String[] args) {
         SingleLinkedList11 sll = new SingleLinkedList11();
+        Scanner sc = new Scanner(System.in);
 
-        Mahasiswa11 mhs1 = new Mahasiswa11("21212203", "Dirga", "4D", 3.6);
-        Mahasiswa11 mhs2 = new Mahasiswa11("22212202", "Cintia", "3C", 3.5);
-        Mahasiswa11 mhs3 = new Mahasiswa11("23212201", "Bimon", "2B", 3.8);
-        Mahasiswa11 mhs4 = new Mahasiswa11("24212200", "Alvaro", "1A", 4.0);
+        System.out.print("Masukkan jumlah data yang ingin ditambahkan:");
+        int jumlahData = sc.nextInt();
+        sc.nextLine(); 
+        for (int i = 0; i < jumlahData; i++) {
+            System.out.println("Masukkan data ke-" + (i + 1) + ":");
+            System.out.print("NIM: ");
+            String nim = sc.nextLine();
+            System.out.print("Nama: ");
+            String nama = sc.nextLine();
+            System.out.print("Kelas: ");
+            String kelas = sc.nextLine();
+            System.out.print("IPK: ");
+            double ipk = sc.nextDouble();
+            sc.nextLine(); 
 
-        sll.print(); 
+            Mahasiswa11 mhs = new Mahasiswa11(nim, nama, kelas, ipk);
+            sll.addLast(mhs); 
+        }
 
-        sll.addFirst(mhs1);
+        System.out.println("\nData dalam Linked List:");
         sll.print();
 
-        sll.addLast(mhs4);
-        sll.print();
-
-        sll.insertAt(2, mhs2); 
-        sll.insertAfter("Cintia", mhs3); 
-        sll.print();
+        sc.close();
     }
 }
